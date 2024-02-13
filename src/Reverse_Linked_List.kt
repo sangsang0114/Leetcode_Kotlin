408c64c6439b6969deecd1210e3431f5b0f48616
yoon <envy15@skuniv.ac.kr>
2024-02-13 21:04:03 +0900

    Reverse_Linked_List.kt

---
+++ src/Reverse_Linked_List.kt
@@ -0,0 +1,12 @@
+fun reverseList(head: ListNode?): ListNode?{
+    var prev: ListNode? = null
+    var node = head
+
+    while(node!=null){
+        val next = node.next
+        node.next = prev
+        prev = node
+        node = next
+    }
+    return prev
+}
